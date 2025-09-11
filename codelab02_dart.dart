abb504bf6f5531caa012598be416b5ea2988b164
CrushedKatana <charelsadewo123@gmail.com>
2025-09-11 21:54:11 +0700

    Create codelab02_dart.dart

---
+++ codelab02_dart.dart
@@ -0,0 +1,12 @@
+// void main() {
+//    for (int i = 0; i < 5; i++) {
+//      print('hello ${i + 1}');
+//    }
+// }
+
+
+void main() {
+   for (int i = 21; i > 9 ; i--) {
+    print('Nama saya adalah Charel, sekarang berumur ${i - 1}');
+  }
+}
