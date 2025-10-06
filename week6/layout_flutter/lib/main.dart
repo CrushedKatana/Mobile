5331697ef7b804f04fb8fd1d7f2e9e1eb51471ab
CrushedKatana <charelsadewo123@gmail.com>
2025-10-06 22:09:37 +0700

    com prak 1

---
+++ week6/layout_flutter/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 🔸 Langkah 4: Implementasi title row
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          // 🟩 Soal 1: Expanded + Column dengan alignment start
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // 🟩 Soal 2: Padding bawah + warna abu-abu
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // 🟩 Soal 3: Icon bintang merah + teks angka 41
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout: Nama Anda - NIM Anda',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [
+            titleSection, // tampilkan hasil layout bagian title
+          ],
+        ),
+      ),
+    );
+  }
+}
